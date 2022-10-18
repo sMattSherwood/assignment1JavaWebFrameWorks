959c394d56f282d3532fc32bcb4febacc07f0454
Matthew Sherwood <94009249+sherwoodMatthew@users.noreply.github.com>
2022-10-18 14:32:07 -0400

    create assignment1

---
+++ src/main/java/assignment1/msherwood/MsherwoodApplication.java
@@ -0,0 +1,13 @@
+package assignment1.msherwood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsherwoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsherwoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assignment1/msherwood/MsherwoodApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment1.msherwood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsherwoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
